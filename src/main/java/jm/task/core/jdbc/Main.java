16f6fcea7097b3007cca8a3367ff6e52b046d059
Klauss_Unsen <kolian1000000@mail.ru>
2024-11-07 16:09:59 +0300

    .

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,10 @@
 package jm.task.core.jdbc;
+import org.hibernate.Version;
 
 import jm.task.core.jdbc.dao.*;
 import jm.task.core.jdbc.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
